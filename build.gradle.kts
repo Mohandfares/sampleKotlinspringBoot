befeb796da362487706c9bfc22604349b18acfa8
Fares <mohand@protid.com>
2021-07-15 10:36:20 +0200

    Spring Security

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-mustache")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
@@ -44,6 +45,7 @@ dependencies {
 	testImplementation("com.ninja-squad:springmockk:3.0.1")
 
 	kapt("org.springframework.boot:spring-boot-configuration-processor")
+
 }
 
 tasks.withType<KotlinCompile> {
