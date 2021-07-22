1eb83470b651ea6f9599fdc1e17ac5bb713cc806
mohandfares <farestecweb@gmail.com>
2021-07-22 23:16:10 +0100

    change kotlin version from 1.5.20 to 1.4.32

--- src/main/kotlin/com/example/sampleKotlinspringBoot/Ext/Extensions.kt
+++ src/main/kotlin/com/example/sampleKotlinspringBoot/Ext/Extensions.kt
@@ -25,7 +25,7 @@ private fun getOrdinal(n: Int) = when {
     else -> "${n}th"
 }
 
-fun String.toSlug() = lowercase()
+fun String.toSlug() = toLowerCase()
     .replace("\n", " ")
     .replace("[^a-z\\d\\s]".toRegex(), " ")
     .split(" ")
